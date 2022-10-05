3c01a1a299425c66f86855ab53ec981bdac7093d
Z70007Z <Z-7000@yandex.ru>
2022-10-06 00:06:05 +0800

    1_3

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -4,13 +4,28 @@
  * */
 const createRequest = (options = {}) => {
 
-    let xhr = new XMLHttpRequest;
-    url: 'http://localhost:8000';
-    data: { // произвольные данные, могут отсутствовать
-        email: 'ivan@poselok.ru';
-        password: 'odinodin';
-    };
-    method: 'GET'; // метод запроса
+    const xhr = new XMLHttpRequest;
+    
+    createRequest({
+        url: 'https://example.com', // адрес
+        data: { // произвольные данные, могут отсутствовать
+          email: 'ivan@poselok.ru',
+          password: 'odinodin'
+        },
+        method: 'GET', // метод запроса
+        /*
+          Функция, которая сработает после запроса.
+          Если в процессе запроса произойдёт ошибка, её объект
+          должен быть в параметре err.
+          Если в запросе есть данные, они должны быть переданы в response.
+        */
+
+
+        callback: (err, response) => {
+          console.log( 'Ошибка, если есть', err );
+          console.log( 'Данные, если нет ошибки', response );
+        }
+      });
 
     createRequest({
         url: 'http://localhost:8000',
