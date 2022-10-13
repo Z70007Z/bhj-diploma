5718173b8c12a36f41f5a737dd5a35c44990b34a
Z70007Z <Z-7000@yandex.ru>
2022-10-13 23:21:10 +0800

    1212

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -12,7 +12,7 @@ class Modal {
    * необходимо выкинуть ошибку.
    * */
   constructor(element){
-
+    this.element = element;
   }
 
   /**
@@ -21,7 +21,22 @@ class Modal {
    * (с помощью метода Modal.onClose)
    * */
   registerEvents() {
-
+    try {
+        // получим все <p> на странице
+      if (this.element) {
+        const elementButton = this.element.querySelectorAll('[data-dismiss="modal"]');
+        elementButton.forEach((elBut) => {
+          elBut.addEventListener('click', (event) => {
+            this.onClose(event);
+          });
+        });
+      } else {
+        throw new SyntaxError("Переданный элемент не существует");
+      }
+        
+    } catch(err) {
+      alert(err.message);
+    }
   }
 
   /**
@@ -29,19 +44,19 @@ class Modal {
    * Закрывает текущее окно (Modal.close())
    * */
   onClose(e) {
-
+    this.close();
   }
   /**
    * Открывает окно: устанавливает CSS-свойство display
    * со значением «block»
    * */
   open() {
-
+    this.element.style.display = 'block';
   }
   /**
    * Закрывает окно: удаляет CSS-свойство display
    * */
   close(){
-
+    this.element.style.display = '';
   }
 }
